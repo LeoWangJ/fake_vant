f7c5533a990079c988891bbd8e63ad0de98eee49
leowang <rfv7855659@gmail.com>
2022-10-29 03:29:30 +0800

    ✨format fix

--- packages/vant-cli/src/index.ts
+++ packages/vant-cli/src/index.ts
@@ -1,8 +1,8 @@
-import fs from 'fs'
-import {URL,fileURLToPath} from 'url'
+import fs from 'fs';
+import { URL, fileURLToPath } from 'url';
 
-const packagePath = fileURLToPath(new URL('../package.json',import.meta.url))
-const packageJson =JSON.parse(fs.readFileSync(packagePath,'utf-8'));
-export const cliVersion:string = packageJson.version
+const packagePath = fileURLToPath(new URL('../package.json', import.meta.url));
+const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
+export const cliVersion: string = packageJson.version;
 
 process.env.FAKE_VANT_CLI_VERSION = cliVersion;
